9609a667e9ea05f43871e76fc4b9a3506ec80724
mamuk <baqarighvaladze@gmail.com>
2024-12-14 19:53:58 +0400

    commit message

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,173 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+/*import java.io.FileInputStream;
+import java.io.IOException;
+ public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String filePath = "numbers.txt";
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            System.out.println("Reading 10 bytes");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            for (int i = 0; i < 10; i++) {
+                int byteValue = fis.read();
+                if (byteValue == -1) {
+                    System.out.println("End of file reached before reading 10 bytes.");
+                    break;
+                }
+                System.out.println("Byte " + (i + 1) + ": " + byteValue);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading " + e.getMessage());
         }
     }
-}
+}*/
+
+/*import java.io.*;
+public class Main {
+    public static void main(String[] args) {
+        String inputFile = "input.txt";
+        String outputFile = "output.txt";
+
+        byte[] numbers = new byte[10];
+        int sum = 0;
+
+        try (FileInputStream fis = new FileInputStream(inputFile)) {
+            for (int i = 0; i < 10; i++) {
+                numbers[i] = (byte) fis.read();
+                sum += numbers[i];
+            }
+        } catch (IOException e) {
+            System.out.println("Error while readin: " + e.getMessage());
+            return;
+        }
+
+        try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+            for (int i = 0; i < 10; i++) {
+                fos.write(numbers[i]);
+            }
+            fos.write(sum);
+        } catch (IOException e) {
+            System.out.println("Error while readin: " + e.getMessage());
+        }
+
+        System.out.println("Succesful.");
+    }
+}*/
+
+
+/*import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String inputFile = "input.txt";
+        String outputFile = "output.txt";
+        char[] characters = new char[10];
+        int sum = 0;
+
+        try (FileReader fr = new FileReader(inputFile)) {
+            for (int i = 0; i < 10; i++) {
+                int value = fr.read();
+                if (value == -1) {
+                    System.out.println("Not enough characters.");
+                    return;
+                }
+                characters[i] = (char) value;
+            }
+        } catch (IOException e) {
+            System.out.println("Error while reading: " + e.getMessage());
+            return;
+        }
+
+        try (FileWriter fw = new FileWriter(outputFile)) {
+            for (int i = 0; i < 10; i++) {
+                fw.write(characters[i]);
+            }
+        } catch (IOException e) {
+            System.out.println("Error while writing: " + e.getMessage());
+        }
+
+        System.out.println("Successful.");
+    }
+}*/
+
+/*import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String outputFile = "numbers.txt";
+
+        int[] numbers = {5, 12, 20, 35, 42};
+
+        try (FileWriter fw = new FileWriter(outputFile)) {
+            fw.write(numbers.length + "\n");
+            for (int i = 0; i < numbers.length; i++) {
+                fw.write(numbers[i] + "\n");
+            }
+
+            System.out.println("Successfully written");
+        } catch (IOException e) {
+            System.out.println("Error while writing: " + e.getMessage());
+        }
+    }
+}*/
+
+/*import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String inputFile = "numbers.txt";
+
+        int negativeCount = 0;
+        int zeroCount = 0;
+        int positiveCount = 0;
+
+        try (Scanner scanner = new Scanner(new File(inputFile))) {
+            while (scanner.hasNextInt()) {
+                int number = scanner.nextInt();
+                if (number < 0) {
+                    negativeCount++;
+                } else if (number == 0) {
+                    zeroCount++;
+                } else {
+                    positiveCount++;
+                }
+            }
+
+            System.out.println("Negative numbers: " + negativeCount);
+            System.out.println("Zero numbers: " + zeroCount);
+            System.out.println("Positive numbers: " + positiveCount);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found.");
+        }
+    }
+}*/
+
+/*import java.io.File;
+public class Main {
+    public static void main(String[] args) {
+
+        String fileName = "numbers.txt";
+        File file = new File(fileName);
+
+        if (file.exists()) {
+            System.out.println("File path: " + file.getAbsolutePath());
+            System.out.println("File size: " + file.length() + " bytes");
+            if (file.isFile()) {
+                System.out.println("It is a file.");
+            }
+            if (file.isDirectory()) {
+                System.out.println("It is a directory.");
+            }
+            System.out.println("Last modified: " + file.lastModified());
+        } else {
+            System.out.println("The file does not exist.");
+        }
+    }
+}*/
+
+
+
+
